10565c737543c12e571b34eec21bc6045ad3302e
KrivoshejaIvan <ranger3331@gmail.com>
2016-04-21 16:38:30 +0300

    modified JSHomework13-14

---
+++ js/QBase.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+ var QBase = {
+    question:  [
+        {
+            q:'Что такое JavaScript',
+                answer: [
+                    {text:'Страна в Африке'},
+                    {text:'Известный Певец'},
+                    {text:'Язык програмирования'}
+                        ]
+        },
+        {
+            q:'С помощью какого тега моно подключить файлы к HTML',
+                answer: [
+                    {text:'File'},
+                    {text:'Link'},
+                    {text:'Upload'}
+                ]
+        }
+    ]
+};
+
+localStorage.setItem('QBase',JSON.stringify(QBase));
+
+var QBase = localStorage.getItem('QBase');
+QBase = JSON.parse(QBase);
+
--- js/script.js
+++ js/script.js
@@ -3,65 +3,63 @@ $(function () {
     var $html = $('html');
     var $body = $('body');
     var $answerLabel = $('.answerLabel');
-    var question1 = {
-        question:'Что такое JavaScript',
-        answer: [
-            'Страна в Африке',
-            'Известный Певец',
-            'Язык програмирования'
-
-        ]
-    };
-    var question2 = {
-        question:'С помощью какого тега моно подключить файлы к HTML',
-        answer: [
-            'File',
-            'Link',
-            'Upload'
-        ]
-    };
-    localStorage.setItem('obj1',JSON.stringify(question1));
-    localStorage.setItem('obj2',JSON.stringify(question2));
-
-
-    var obj1 = localStorage.getItem('obj1');
-    obj1 = JSON.parse(obj1);
-    var obj2 = localStorage.getItem('obj2');
-    obj2 = JSON.parse(obj2);
-
-    var resultAnswer1 = [obj1];
-    var resultAnswer2 = [obj2];
-
     var html = $('#test').html();
+    var tmpl =  $('#test').html();
+    tmpl = _.template(tmpl);
 
-    var content =tmpl(html,{
-        data: resultAnswer1,
-        dataSecond: resultAnswer2
+    var content = tmpl(html,{
+        QBase: QBase
     });
+
 
     $('body').append(content);
 
+    for(var i=0;i<QBase.question.length; i++){
+        $('.answer_checkbox'+i).addClass('disabled'+i);
+        $('.disabled0').on('change',function(){
+            if(this.checked){
+                $(this).removeClass('disabled0');
+                $('.disabled0').attr('disabled','disabled');
+            } else {
+                $(this).addClass('disabled0');
+                $('.disabled0').removeAttr('disabled');
+            }
+        });
+        $('.disabled1').on('change',function(){
+            if(this.checked){
+                $(this).removeClass('disabled1');
+                $('.disabled1').attr('disabled','disabled');
+            } else {
+                $(this).addClass('disabled1');
+                $('.disabled1').removeAttr('disabled');
+            }
+        });
+    }
+
     var check1;
     var check2;
-
-    $('#check1_3').on('click', function () {
+    var check3;
+    $('#chBox_0_2').on('click', function () {
         check1 = true
     });
-    $('#check2_2').on('click', function () {
+    $('#chBox_1_1').on('click', function () {
         check2 = true
     });
 
-    $('input[type=checkbox]').change(function(){
-        $(this).parent().siblings().children().filter(':checked').not(this).removeAttr('checked');
-    });
+
+
+
     $('.answerButton').on('click', function (e) {
+
         e.preventDefault();
         if (check1 && check2 == true) {
             alert('И это правильный ответ');
             location.reload();
-        } else {
+        }   else {
             alert('Вы слабое звено');
             location.reload();
         }
+
+
     });
 });
